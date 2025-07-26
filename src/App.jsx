be943bc53acc9926ea7bbecb8318b0806e187f4c
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-26 18:42:26 +0530

    adding prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,37 @@
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import { lazy, React, Suspense } from "react";
-import "./App.css";
-const Dashboard = lazy(() => import("./components/Dashboard"));
-const Landing = lazy(() => import("./components/Landing"));
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Dashboard />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Landing />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <Count count={count} />
+      <Buttons count={count} setCount={setCount} />
     </div>
   );
 }
 
-function Appbar() {
-  const navigate = useNavigate();
+function Count({ count }) {
+  return <div>{count}</div>;
+}
+
+function Buttons({ count, setCount }) {
   return (
     <div>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Landing page
-        </button>
-
-        <button
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-        >
-          Dashboard
-        </button>
-      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
     </div>
   );
 }
-
 export default App;
